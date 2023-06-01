52b67978c7dbfbf071e1e1b71088af02fd996379
ravip7604 <135264428+ravip7604@users.noreply.github.com>
2023-06-02 00:38:27 +0530

    Add files via upload

---
+++ Assets/scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseResume : MonoBehaviour
+{
+
+    public GameObject PauseScreen;
+    //public GameObject PauseButton;
+
+    bool GamePaused;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GamePaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GamePaused)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+    }
+
+    public void PauseGame()
+    {
+        GamePaused = true;
+        PauseScreen.SetActive(true);
+        //PauseButton.SetActive(false);
+    }
+
+    public void ResumeGame()
+    {
+        GamePaused = false;
+        PauseScreen.SetActive(false);
+        //PauseButton.SetActive(true);
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
